ed73af5711e99fe728fac22b590ec385ee2ff5c3
har-y <jakub.harenda@gmail.com>
2020-03-04 21:02:50 +0100

    Add: Player - Movement Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float _speed = 10f;
+    private float _positionValue = 2.65f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");
+        Vector2 currentPosition = transform.position;
+
+        if (_horizontal > 0)
+        {
+            currentPosition.x += _speed * Time.deltaTime;
+        }
+        else if (_horizontal < 0)
+        {
+            currentPosition.x -= _speed * Time.deltaTime;
+        }
+
+        if (currentPosition.x < -_positionValue)
+        {
+            currentPosition.x = -_positionValue;
+        }
+
+        if (currentPosition.x > _positionValue)
+        {
+            currentPosition.x = _positionValue;
+        }
+
+        transform.position = currentPosition;
     }
 }
