962e7448fb7be2fe877fe106aec7d4d29d7de908
har-y <jakub.harenda@gmail.com>
2020-03-04 23:12:10 +0100

    Add: Collector - Score Text Script

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collector : MonoBehaviour
 {
+    [SerializeField] private Text _text;
+
     private int _score = 0;
 
     // Start is called before the first frame update
@@ -15,15 +18,21 @@ public class Collector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Enemy")
         {
-            _score++;
+            IncreaseScore();
             Destroy(collision.gameObject);
         }
+    }
+
+    private void IncreaseScore()
+    {
+        _score++;
+        _text.text = "Score: " + _score;
     }
 }
